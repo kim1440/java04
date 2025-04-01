9371c3db02cdc41d9b999433ebbade5634b5d176
kimdoyeong_laptop <wah8138@cju.ac.kr>
2025-04-01 11:24:50 +0900

    Scanner

---
+++ Calc/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    private static int num;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫번째 숫자를 입력하시오.: ");
+        int num1 = sc.nextInt();
+
+ /*       System.out.print("연산 방법: ");
+        String a= sc.next();
+*/
+        System.out.print("두번째 숫자를 입력하시오.: ");
+        int num2 = sc.nextInt();
+
+        System.out.println(num1+num2);
+    }
+}
