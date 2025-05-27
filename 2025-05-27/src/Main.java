f877c5fc0dd81b21ef88aa9732cf35ec4e7fed3e
kimdoyeong_laptop <wah8138@cju.ac.kr>
2025-05-27 10:49:59 +0900

    05-27

---
+++ 2025-05-27/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    int radius;
+
+    public void setRadius(int r) {
+        radius = r;
+    }
+
+    public double calculateArea() {
+        return 3.14 * radius * radius;
+    }
+
+    public void printCircle() {
+        System.out.println("반지름 : " + radius);
+        System.out.println("넓이 : " + calculateArea());
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.setRadius(5);
+        main.printCircle();
+    }
+}
